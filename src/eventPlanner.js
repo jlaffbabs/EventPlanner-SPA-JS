9cf88c516894c32761c0bff74106adc8e5fa7bfb
Shaneil Clarke <shaneilske@yahoo.com>
2019-01-22 15:52:54 +0000

    added listEvents method

--- src/eventPlanner.js
+++ src/eventPlanner.js
@@ -6,4 +6,13 @@ class EventPlanner {
   addEvent (text, date, time) {
     this.events.push([text, date, time]);
   };
+
+  listEvents(){
+    for(let i = 0; i < this.events.length; i++){
+    console.log(this.events[i][0])
+    console.log(this.events[i][1])
+    console.log(this.events[i][2])
+  }
+  }
+
 };
